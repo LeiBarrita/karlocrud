47081a31a99260005d2b2ef70d144ffd556db370
Uziel Barrita <leibarrita@gmail.com>
2025-01-12 12:42:32 -0600

    Order - State association

--- backend/src/database/models/order.js
+++ backend/src/database/models/order.js
@@ -1,23 +1,23 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Order extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Order.belongsTo(models.State, {
+        foreignKey: "stateId",
+        as: "state",
+      });
     }
   }
-  Order.init({
-    total: DataTypes.FLOAT
-  }, {
-    sequelize,
-    modelName: 'Order',
-  });
+  Order.init(
+    {
+      total: DataTypes.FLOAT,
+    },
+    {
+      sequelize,
+      modelName: "Order",
+    }
+  );
   return Order;
 };
--- backend/src/database/models/state.js
+++ backend/src/database/models/state.js
@@ -1,23 +1,23 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class State extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      State.hasMany(models.Order, {
+        foreignKey: "stateId",
+        as: "orders",
+      });
     }
   }
-  State.init({
-    status: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'State',
-  });
+  State.init(
+    {
+      status: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "State",
+    }
+  );
   return State;
 };
