0aa1e76919c1bbc52de5184b317bd9515a9e2257
Pedro Uziel Barrita Licea <leibarrita@gmail.com>
2025-01-13 12:15:24 -0600

    Home page base

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,6 +1,6 @@
-const Button = ({ text }: { text: string }) => {
+const Button = ({ text, onClick }: { text: string; onClick?: () => void }) => {
   return (
-    <button className="p-1 bg-emerald-400" type="button">
+    <button className="p-1 bg-emerald-400" type="button" onClick={onClick}>
       {text}
     </button>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,20 @@
+import ProductCard from "../components/ProductCard";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className="bg-emerald-100 p-10 flex flex-col min-h-lvh w-lvw text-black">
+      <h1 className="my-8 self-center">Karlo Store</h1>
+      <h2 className="text-4xl mb-6">Tienda 1</h2>
+      <div className="flex py-2 gap-3 overflow-x-auto">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+    </main>
+  );
 };
 
 export default Home;
