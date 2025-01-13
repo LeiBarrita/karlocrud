10a44407cef848d554c340be4b4b1e43828cee68
Pedro Uziel Barrita Licea <leibarrita@gmail.com>
2025-01-13 11:02:39 -0600

    base routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
 import Login from "./components/Login";
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Routes>
+      <Route index element={<Login />} />
+      <Route path="register" element={<div>Register</div>} />
+
+      <Route path="client">
+        <Route index element={<div>Client Home</div>} />
+        <Route path="cart" element={<div>Client Cart</div>} />
+        <Route path="orders" element={<div>Client Orders</div>} />
+      </Route>
+
+      <Route path="business">
+        <Route index element={<div>Business Home</div>} />
+        <Route path="products" element={<div>Business Products</div>} />
+        <Route path="orders" element={<div>Business Orders</div>} />
+      </Route>
+    </Routes>
   );
 }
 
