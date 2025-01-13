5c33c8c7801a8fab2ba2202a4124011d31433194
Pedro Uziel Barrita Licea <leibarrita@gmail.com>
2025-01-13 13:01:16 -0600

    base cart page

---
+++ frontend/src/components/CartItem.tsx
@@ -0,0 +1,16 @@
+const CartItem = () => {
+  return (
+    <div className="bg-white flex h-16 items-center gap-6 pr-4 rounded-sm overflow-hidden">
+      <img
+        className="object-cover h-full"
+        src="https://i.pinimg.com/474x/dc/95/b9/dc95b95c90c71fd83e88369c31ecce8b.jpg"
+        alt="product image"
+      />
+      <h2 className="text-3xl">Producto</h2>
+      <h2 className="text-3xl">$ 899.00</h2>
+      <h2 className="text-3xl">x 4</h2>
+    </div>
+  );
+};
+
+export default CartItem;
--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,5 +1,23 @@
+import Button from "../components/Button";
+import CartItem from "../components/CartItem";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <main className="bg-emerald-100 p-10 flex flex-col min-h-lvh w-lvw text-black items-center">
+      <h1 className="my-8">Carrito</h1>
+
+      <div className="flex flex-col gap-6">
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+
+        <Button text="Comprar" />
+      </div>
+    </main>
+  );
 };
 
 export default Cart;
